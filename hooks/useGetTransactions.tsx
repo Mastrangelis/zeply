6e1fca21ad7ccc005dd0165cbbcd0cd0fe1e0b8f
Nikos Mastrangelis <n.mastrangelis.ext@tradias.com>
2023-02-21 11:20:02 +0200

    increase loading time

--- hooks/useGetTransactions.tsx
+++ hooks/useGetTransactions.tsx
@@ -1,46 +1,53 @@
-"use client";
+'use client';
 
-import { useEffect, useState } from "react";
-import { areArraysEqual } from "@/utils";
+import { useEffect, useState } from 'react';
+import { areArraysEqual } from '@/utils';
 import {
-  useGetLatestBlockQuery,
-  useGetTxnsDetailsQuery,
-} from "@/components/api/queries";
+    useGetLatestBlockQuery,
+    useGetTxnsDetailsQuery
+} from '@/components/api/queries';
 
 export default function useGetTransactions() {
-  const [latestTxnIdxs, setLatestTxnIdxs] = useState<number[]>([]);
+    const [customLoading, setCustomLoading] = useState<boolean>(true);
+    const [latestTxnIdxs, setLatestTxnIdxs] = useState<number[]>([]);
 
-  const { data: latestBlock, isLoading: isLatestBlockLoading } =
-    useGetLatestBlockQuery();
+    const { data: latestBlock, isLoading: isLatestBlockLoading } =
+        useGetLatestBlockQuery();
 
-  useEffect(() => {
-    if (!isLatestBlockLoading && latestBlock?.txIndexes) {
-      const { txIndexes } = latestBlock;
-      const lastFiveTxIndexes = txIndexes.slice(0, 5);
-      if (!areArraysEqual(lastFiveTxIndexes, latestTxnIdxs)) {
-        setLatestTxnIdxs(lastFiveTxIndexes);
-      }
-    }
-  }, [isLatestBlockLoading, latestBlock]);
+    useEffect(() => {
+        if (!isLatestBlockLoading && latestBlock?.txIndexes) {
+            const { txIndexes } = latestBlock;
+            const lastFiveTxIndexes = txIndexes.slice(0, 5);
+            if (!areArraysEqual(lastFiveTxIndexes, latestTxnIdxs)) {
+                setLatestTxnIdxs(lastFiveTxIndexes);
+            }
+        }
+    }, [isLatestBlockLoading, latestBlock]);
 
-  // Get details for the last 5 transactions
-  const results = useGetTxnsDetailsQuery(latestTxnIdxs);
+    // Get details for the last 5 transactions
+    const results = useGetTxnsDetailsQuery(latestTxnIdxs);
 
-  // Check if any query for any transaction is still loading
-  const isLoading = results.some((result) => result.isLoading);
+    // Check if any query for any transaction is still loading
+    const isLoading = results.some((result) => result.isLoading);
 
-  // Check if any query for any transaction had error
-  const isError = results.some((result) => result.isError);
+    useEffect(() => {
+        if (!isLoading) {
+            setTimeout(() => setCustomLoading(false), 2500);
+        }
+    }, [isLoading]);
 
-  // Merge transations details
-  const data = results
-    .map((result) => result.data)
-    .sort((a, b) => b.time - a.time);
+    // Check if any query for any transaction had error
+    const isError = results.some((result) => result.isError);
 
-  return {
-    latestBlock,
-    data,
-    isError,
-    isLoading,
-  };
+    // Merge transations details
+    const data = results
+        .map((result) => result.data)
+        .sort((a, b) => b.time - a.time);
+
+    return {
+        latestBlock,
+        data,
+        isError,
+        customLoading
+    };
 }
--- sections/Tabs/RecentTxns.tsx
+++ sections/Tabs/RecentTxns.tsx
@@ -1,54 +1,56 @@
-import React, { useMemo } from "react";
-import useGetTransactions from "@/hooks/useGetTransactions";
-import { AccordionCard } from "@/components/Cards";
-import { FormattedTxn, Txn } from "@/types/blockchain";
-import { formatTxn } from "@/utils/blockchain";
-import { Item } from "@/components";
-import { Filter } from "constants/enums";
-import { useSelectCurrencyContext } from "context/SelectCurrencyContext";
+import React, { useMemo } from 'react';
+import useGetTransactions from '@/hooks/useGetTransactions';
+import { AccordionCard } from '@/components/Cards';
+import { FormattedTxn, Txn } from '@/types/blockchain';
+import { formatTxn } from '@/utils/blockchain';
+import { Item } from '@/components';
+import { Filter } from 'constants/enums';
+import { useSelectCurrencyContext } from 'context/SelectCurrencyContext';
 
 const RecentTxns = () => {
-  const {
-    latestBlock,
-    data: transactions,
-    isLoading: isTransactionsLoading,
-  } = useGetTransactions();
+    const {
+        latestBlock,
+        data: transactions,
+        customLoading: isTransactionsLoading
+    } = useGetTransactions();
 
-  const { selectedCurrency, prices } = useSelectCurrencyContext();
+    const { selectedCurrency, prices } = useSelectCurrencyContext();
 
-  const items: FormattedTxn[] = useMemo(() => {
-    if (isTransactionsLoading) return new Array(5).fill({});
+    const items: FormattedTxn[] = useMemo(() => {
+        if (isTransactionsLoading) return new Array(5).fill({});
 
-    return transactions?.map((transaction: Txn) =>
-      formatTxn(transaction, latestBlock, prices, selectedCurrency)
+        return transactions?.map((transaction: Txn) =>
+            formatTxn(transaction, latestBlock, prices, selectedCurrency)
+        );
+    }, [transactions, isTransactionsLoading]);
+
+    const renderAccordionCardHeader = (item: FormattedTxn) => (
+        <div className="flex items-center justify-between mr-2">
+            <span className="font-medium text-base">
+                Details for Transaction with hash{' '}
+                <span className="font-bold text-disabled">{item?.hash}</span>
+            </span>
+            {item?.time && (
+                <span>{new Date(item.time * 1000).toISOString()}</span>
+            )}
+        </div>
     );
-  }, [transactions, isTransactionsLoading]);
 
-  const renderAccordionCardHeader = (item: FormattedTxn) => (
-    <div className="flex items-center justify-between mr-2">
-      <span className="font-medium text-base">
-        Details for Transaction with hash{" "}
-        <span className="font-bold text-disabled">{item?.hash}</span>
-      </span>
-      {item?.time && <span>{new Date(item.time * 1000).toISOString()}</span>}
-    </div>
-  );
-
-  return (
-    <>
-      {items?.map((item: FormattedTxn, idx: number) => (
-        <AccordionCard
-          key={idx}
-          isLoading={isTransactionsLoading}
-          header={renderAccordionCardHeader(item)}
-        >
-          <div key={idx}>
-            <Item item={item} type={Filter.TRANSACTION} />
-          </div>
-        </AccordionCard>
-      ))}
-    </>
-  );
+    return (
+        <>
+            {items?.map((item: FormattedTxn, idx: number) => (
+                <AccordionCard
+                    key={idx}
+                    isLoading={isTransactionsLoading}
+                    header={renderAccordionCardHeader(item)}
+                >
+                    <div key={idx}>
+                        <Item item={item} type={Filter.TRANSACTION} />
+                    </div>
+                </AccordionCard>
+            ))}
+        </>
+    );
 };
 
 export default RecentTxns;
